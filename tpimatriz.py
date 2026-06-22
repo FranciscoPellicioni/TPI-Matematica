16a1870fc11771c51cf1ff96bc7fc8d4ca7015aa
Fabrizio Perazzi <fabriziopemarcomini@gmail.com>
2026-06-22 02:35:02 -0300

    termine de desarrollar funcion recorrer matriz

--- tpimatriz.py
+++ tpimatriz.py
@@ -1,23 +1,46 @@
 def inicializar():
     matriz = []
-    acu = 0
-    funciones = int(input("ingrese la cantidad de funciones"))
-    servidores = int(input("ingrese la cantidad de servidores"))
+    funciones = int(input("Ingrese la cantidad de funciones: "))
+    servidores = int(input("Ingrese la cantidad de servidores: "))
     for i in range(funciones):
-        matriz.append([0]*servidores)
-    cargamat(matriz,servidores,funciones)
-    recorrermat(matriz,funciones,servidores,acu)
-def cargamat(matriz,servidores,funciones):
+        matriz.append([0] * servidores)
+    print("Matriz inicial:")
+    mostramat(matriz)
+    cargamat(matriz, servidores, funciones,)
+    recorrermat(matriz, funciones, servidores)
+def cargamat(matriz, servidores, funciones):
     for i in range(funciones):
         for j in range(servidores):
-            matriz[i][j] = int(input("ingrese el elemento %d,%d :"%(i,j)))
-def recorrermat(matriz,funciones,servidores,acu): 
-    valor = 0
+            matriz[i][j] = int(input("Ingrese el elemento %d,%d: " % (i, j)))
+            print("Matriz actualizada:")
+            mostramat(matriz)    
+def recorrermat(matriz, funciones, servidores):
+    print("Matriz final:")
+    mostramat(matriz)
+    mostratranspuesta(matriz)
     for i in range(funciones):
-        acu = 0 
+        acu = 0
         for j in range(servidores):
-            if matriz[i][j] >=0:
-                acu = matriz[i][j] + acu
-    valor
-    print(acu)
+            acu = acu + matriz[i][j]
+        promefun = acu / servidores
+        print("Fila", i, "el tiempo promedio es:", promefun)
+    for i in range(servidores):
+        acu2 = 0
+        for j in range(funciones):
+            acu2 = acu2 + matriz[j][i]
+        promeser = acu2 / funciones
+        print("Columna", i, "el tiempo promedio es:", promeser)
+def mostramat(matriz):
+    for i in range(len(matriz)):
+        print(matriz[i])
+    print()
+def mostratranspuesta(matriz):
+    transpuesta = []
+    for j in range(len(matriz)):
+        fila = []
+        for i in range(len(matriz)):
+            fila.append(matriz[i][j])
+        transpuesta.append(fila)
+    print("Matriz transpuesta:")
+    mostramat(transpuesta)
 inicializar()
